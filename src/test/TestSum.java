358ec885bf109e7525454cd132eb6fce9776c3c0
davreymar <davreymar@alum.us.es>
2017-01-13 12:08:51 +0100

    Añadiendo conflicto

--- src/test/TestSum.java
+++ src/test/TestSum.java
@@ -20,7 +20,7 @@ public class TestSum extends TestCase {
 	}
 
 	public void testProdDosNegativos() {
-		assertTrue(2 == MathMethods.sum(-1, -2));
+		assertTrue(5 == MathMethods.sum(-1, -5));
 	}
 	
 	
